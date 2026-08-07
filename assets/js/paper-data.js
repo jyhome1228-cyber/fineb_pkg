83d14c5430b7bef87c93a87a75f2e7afbcd240cc
JAEYOUNG PARK <jyhome1228@gmail.com>
2026-08-08 00:03:34 +0900

    Add shared paper material data for quote and sample

---
+++ assets/js/paper-data.js
@@ -0,0 +1,24 @@
+window.FINEB_PAPER_MATERIALS = [
+  {id:'ab',name:'AB / 로얄아이보리',gsm:[300,350,400],note:'패키지 보드',tone:'ivory'},
+  {id:'iv',name:'IV / 아이보리',gsm:[300,350,400],note:'백색 패키지 보드',tone:'white'},
+  {id:'ccp',name:'CCP',gsm:[300,350,400],note:'고광택 코팅지 계열',tone:'gloss'},
+  {id:'sc',name:'SC 마닐라',gsm:[300,350,400],note:'범용 패키지 보드',tone:'gray'},
+  {id:'eco-kraft',name:'뉴에코크라프트',gsm:[300,337],note:'크라프트',tone:'kraft'},
+  {id:'eco-black',name:'뉴에코블랙',gsm:[300,350,400],note:'블랙 보드',tone:'black'},
+  {id:'earth',name:'얼스팩',gsm:[295],note:'친환경 패키지지',tone:'earth'},
+  {id:'moon',name:'문보드',gsm:[310,350,450,550],note:'고급 특수지',tone:'moon'},
+  {id:'bamboo-n',name:'밤부팩 N',gsm:[300,350],note:'대나무 펄프 계열',tone:'bamboo-n'},
+  {id:'bamboo-w',name:'밤부팩 W',gsm:[300,350],note:'대나무 펄프 계열',tone:'bamboo-w'},
+  {id:'acopack-ew',name:'아코팩 EW',gsm:[300,350,400],note:'패키지 특수지',tone:'acopack-ew'},
+  {id:'acopack-ww',name:'아코팩 WW',gsm:[300,350,400],note:'패키지 특수지',tone:'acopack-ww'},
+  {id:'oldmill-b',name:'올드밀 B',gsm:[300,350],note:'특수지',tone:'oldmill-b'},
+  {id:'oldmill-ew',name:'올드밀 EW',gsm:[300,350],note:'특수지',tone:'oldmill-ew'},
+  {id:'oldmill-pw',name:'올드밀 PW',gsm:[300,350],note:'특수지',tone:'oldmill-pw'}
+];
+
+window.FINEB_CORR_MATERIALS = [
+  {id:'e',name:'E골',gsm:[],note:'골판지 기본 규격',tone:'corr-e'},
+  {id:'b',name:'B골',gsm:[],note:'골판지 기본 규격',tone:'corr-b'},
+  {id:'e-lam',name:'E골 합지',gsm:[],note:'칼라박스용 합지',tone:'corr-lam'},
+  {id:'b-lam',name:'B골 합지',gsm:[],note:'칼라박스용 합지',tone:'corr-lam'}
+];
